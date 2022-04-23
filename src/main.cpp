ed2ef43a6136534c19e897d1135f4574677d6d3c
Mark <78104595+m42km@users.noreply.github.com>
2022-04-23 09:55:52 -0400

    Code refactoring and a heavy bullet dodged

--- src/main.cpp
+++ src/main.cpp
@@ -6,48 +6,73 @@ void c_system(string cmd) {
     system(cmd.c_str());
 }
 
-int main(int argc, char** argv)
-{
-    string gdSongFolder = "GeometryDash";
+int main(int argc, char** argv) {
+
+    system("title GD Song Manager");
+
+    string gdSongFolder = "%localappdata%\\GeometryDash"; // TODO: Make this a setting (excluding %localappdata%)
+
     if (argc == 1) {
+
+            /* No arguments?
+    ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
+    ⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
+    ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
+    ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
+    ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
+    ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ */
+
         cout << "Please use the song manager by clicking on a .gdsong file..." << endl;
-        system("pause");
 
     } else {
-        string songLocation = argv[1];
+        string songLocation = argv[1]; // TODO: Check if file exists, prob gonna do that in next version
 
         // and now: the code i just copied from stackoverflow
         // https://stackoverflow.com/a/14266139
 
 
-        string songID = songLocation.substr(0, songLocation.find("."));
-        /*                                             ^ might have to change this up,
-                                                         since folders can have . in them
-                                                         but im too lazy
-                                                                                            */
+        string songID_old = songLocation;
 
         string splitChars = "\\";
         string curr;
         size_t pos = 0;
 
-        while ((pos = songID.find(splitChars)) != string::npos) {
-            curr = songID.substr(0, pos);
-            songID.erase(0, pos + splitChars.length());
+        while ((pos = songID_old.find(splitChars)) != string::npos) {
+            curr = songID_old.substr(0, pos);
+            songID_old.erase(0, pos + splitChars.length());
         }
-        // then we will end up with just the song ID, that's it ^o^
+
+        string songID = songID_old.substr(0, songID_old.find("."));
+        /*                                             ^ Just put this here
+                                                         It should fix everything i think */
+
+        // then we will end up with just the song ID, that's it :D
+
+        c_system("title GD Song Manager - " + songID);
 
         cout << "Installing " << songLocation << " @ " << songID << " ..." << endl;
         cout << "=============================================================================" << endl;
 
-        c_system("copy " + songLocation + " %localappdata%\\GeometryDash /Y"); // copy the song, use overwrite flag
-        c_system("del %localappdata%\\GeometryDash\\" + songID + ".mp3 > nul"); // delete old song if it exists
-        c_system("rename %localappdata%\\GeometryDash\\" + (songID + ".gdsong") + " " + (songID + ".mp3")); // finally, rename the song
+        c_system("copy " + songLocation + " " + gdSongFolder + " /Y"); // copy the song, use overwrite flag
+        c_system("del " + gdSongFolder + "\\" + songID + ".mp3 > nul"); // delete old song if it exists
+        c_system("rename " + gdSongFolder + "\\" + (songID + ".gdsong") + " " + (songID + ".mp3")); // finally, rename the song
+
+        // c_system("del " + songLocation); //  TODO: make this a setting or somethin
 
         cout << "=============================================================================" << endl;
         cout << "Song has (probably) been installed. Re-enter the level/restart GD if needed" << endl;
         cout << "This window will close in a couple of seconds, goodbye ..." << endl;
 
-        system("ping 127.0.0.1 -n 5 > nul"); // wait a little bit by doing it the good 'ol script kiddie method
     }
+
+    system("pause");
+
     return 0;
 }
